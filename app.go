bb3e72368f8bd3525543a8e7604beb18cd8669f3
Rupert Bedford <rupert@rupertb.com>
2016-08-22 20:29:23 +0100

    Add config file option

--- app.go
+++ app.go
@@ -1,10 +1,45 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
+	"os"
 )
+
+const defaultPort int = 8080
+
+type Config struct {
+	Id     int
+	Secret string
+	Port   int
+}
+
+func readConfig(filename string) (Config, error) {
+	var config Config
+	var err error
+
+	_, err = os.Stat(filename)
+
+	if err != nil {
+		return config, err
+	}
+
+	_, err = toml.DecodeFile(filename, &config)
+
+	if err != nil {
+		return config, err
+	}
+
+	if config.Port == 0 {
+		config.Port = defaultPort
+	}
+
+	return config, err
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("templates/index.html")
@@ -12,6 +47,15 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var configFilename = flag.String("conf", "config.toml", "path to config file")
+	flag.Parse()
+
+	config, err := readConfig(*configFilename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", indexHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil))
 }
