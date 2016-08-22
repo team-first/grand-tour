1be3368a074b19d5c4fccf295429c5b7fb15c317
Ed Baunton <ebaunton1@bloomberg.net>
2016-08-22 16:06:44 -0400

    Fix indentation and hard tabs

--- backend/get_top_efforts.go
+++ backend/get_top_efforts.go
@@ -3,32 +3,32 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"time"
+    "flag"
+    "fmt"
+    "os"
+    "time"
 
-	"github.com/strava/go.strava"
+    "github.com/strava/go.strava"
 )
 
 
 func main() {
-	var accessToken string
+    var accessToken string
 
-	// Provide an access token, with write permissions.
-	// You'll need to complete the oauth flow to get one.
-	flag.StringVar(&accessToken, "token", "", "Access Token")
+    // Provide an access token, with write permissions.
+    // You'll need to complete the oauth flow to get one.
+    flag.StringVar(&accessToken, "token", "", "Access Token")
 
-	flag.Parse()
+    flag.Parse()
 
-	if accessToken == "" {
-		fmt.Println("\nPlease provide an access_token, one can be found at https://www.strava.com/settings/api")
+    if accessToken == "" {
+        fmt.Println("\nPlease provide an access_token, one can be found at https://www.strava.com/settings/api")
 
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
 
-	client := strava.NewClient(accessToken)
+    client := strava.NewClient(accessToken)
     // make a map per athlete to their list of efforts
     var effortsMap = make(map[int64] [4] int)
     // var segments = []int64 {
@@ -47,27 +47,27 @@ func main() {
         for i := 1;;i++ {
 
             results, err := strava.NewSegmentsService(client).ListEfforts(int64(segmentId)).DateRange(time.Now().Add(-30*24*time.Hour),time.Now()).PerPage(100).Page(i).Do()
-         	if err != nil {
-				fmt.Println(err)
-				fmt.Printf("Break\n")
-				break
-       	    }
+             if err != nil {
+                fmt.Println(err)
+                fmt.Printf("Break\n")
+                break
+               }
 
-        	for _, e := range results {
-	        	fmt.Printf("%5d: %5d %5d\n", e.Id, e.MovingTime, e.Athlete.Id)
+            for _, e := range results {
+                fmt.Printf("%5d: %5d %5d\n", e.Id, e.MovingTime, e.Athlete.Id)
 
-				cur, ok := effortsMap[e.Athlete.Id]
+                cur, ok := effortsMap[e.Athlete.Id]
 
                 if !ok {
-					var a [4] int
-					a[segmentIdx] = e.MovingTime
-					effortsMap[e.Athlete.Id] = a
+                    var a [4] int
+                    a[segmentIdx] = e.MovingTime
+                    effortsMap[e.Athlete.Id] = a
                 } else if cur[segmentIdx] > e.MovingTime {
-					fmt.Printf("beat! %5d %5d\n", cur[segmentIdx], e.MovingTime)
-					cur[segmentIdx] = e.MovingTime
-					effortsMap[e.Athlete.Id] = cur
-				}
-			}
-		}
-	}
+                    fmt.Printf("beat! %5d %5d\n", cur[segmentIdx], e.MovingTime)
+                    cur[segmentIdx] = e.MovingTime
+                    effortsMap[e.Athlete.Id] = cur
+                }
+            }
+        }
+    }
 }
