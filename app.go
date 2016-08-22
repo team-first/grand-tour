e4c87bd5fdeacaf6e9a72ebcd4984a7d33050890
Rupert Bedford <rupert@rupertb.com>
2016-08-22 20:49:27 +0100

    Add oauth

--- app.go
+++ app.go
@@ -4,19 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"github.com/BurntSushi/toml"
+	"github.com/strava/go.strava"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-const defaultPort int = 8080
+const (
+	defaultHost string = "localhost"
+	defaultPort int    = 8080
+)
 
 type Config struct {
 	Id     int
 	Secret string
+	Host   string
 	Port   int
 }
+
+type Params struct {
+	Url string
+}
+
+var authenticator *strava.OAuthAuthenticator
 
 func readConfig(filename string) (Config, error) {
 	var config Config
@@ -34,6 +45,10 @@ func readConfig(filename string) (Config, error) {
 		return config, err
 	}
 
+	if config.Host == "" {
+		config.Host = defaultHost
+	}
+
 	if config.Port == 0 {
 		config.Port = defaultPort
 	}
@@ -42,8 +57,10 @@ func readConfig(filename string) (Config, error) {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	url := authenticator.AuthorizationURL("state1", strava.Permissions.Public, true)
+	params := &Params{Url: url}
 	t, _ := template.ParseFiles("templates/index.html")
-	t.Execute(w, nil)
+	t.Execute(w, params)
 }
 
 func main() {
@@ -56,6 +73,29 @@ func main() {
 		log.Fatal(err)
 	}
 
+	strava.ClientId = config.Id
+	strava.ClientSecret = config.Secret
+
+	authenticator = &strava.OAuthAuthenticator{
+		CallbackURL:            fmt.Sprintf("http://%s:%d/callback", config.Host, config.Port),
+		RequestClientGenerator: nil,
+	}
+
+	path, err := authenticator.CallbackPath()
+
+	http.HandleFunc(path, authenticator.HandlerFunc(oAuthSuccess, oAuthFailure))
+
 	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil))
+}
+
+func oAuthSuccess(auth *strava.AuthorizationResponse, w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/success.html")
+	t.Execute(w, nil)
+}
+
+func oAuthFailure(err error, w http.ResponseWriter, r *http.Request) {
+	// TODO show error message
+	t, _ := template.ParseFiles("templates/failure.html")
+	t.Execute(w, nil)
 }
